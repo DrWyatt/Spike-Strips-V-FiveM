723015afa093574e162773b0d43cf3765678d418
Guilherme R. L <jake90wyatt@gmail.com>
2017-12-19 23:00:45 -0200

    Commit #003 - Major Changes #1
    Added cop check w/Police Management
    Changed Keys
    Added Lane Spikes
    Fixed Code Syntax (According to My Standards)
    Removed Debug Functions
    Added more checks
    Switched from "Natives" to "Natives.API" in most part.

---
+++ Spike Strips V FiveM/PoliceManagement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace Spike_Strips_V
+{
+    public class PoliceManagement : BaseScript
+    {
+        public bool isCop = false;
+        public bool isAdmin = false;
+        public string pluginName;
+
+        public async void Checks(string pluginAcronym)
+        {
+            pluginName = pluginAcronym;
+            EventHandlers.Add(pluginName + ":isCop", new Action(IsCop));
+            EventHandlers.Add(pluginName + ":isNotCop", new Action(IsNotCop));
+            EventHandlers.Add(pluginName + ":isAdmin", new Action(IsAdmin));
+            EventHandlers.Add(pluginName + ":isNotAdmin", new Action(IsNotAdmin));
+            while (true)
+            {
+                await Delay(1000);
+                TriggerServerEvent("pm:isCop", GetPlayerServerId(PlayerId()), pluginName+":isCop", pluginName+":isNotCop");
+                TriggerServerEvent("pm:isAdmin", GetPlayerServerId(PlayerId()), pluginName + ":isAdmin", pluginName + ":isNotAdmin");
+            }
+        }
+
+        public void IsCop()
+        {
+            isCop = true;
+        }
+
+        public void IsNotCop()
+        {
+            isCop = false;
+        }
+
+        public void IsAdmin()
+        {
+            isAdmin = true;
+        }
+
+        public void IsNotAdmin()
+        {
+            isAdmin = false;
+        }
+    }
+}
--- Spike Strips V FiveM/SpikeStripsScript.cs
+++ Spike Strips V FiveM/SpikeStripsScript.cs
@@ -1,131 +1,164 @@
-﻿namespace Spike_Strips_V
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.UI;
+using static CitizenFX.Core.Native.API;
+using CitizenFX.Core.Native;
+
+namespace Spike_Strips_V
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-
-    using CitizenFX.Core;
-    using CitizenFX.Core.UI;
-    using CitizenFX.Core.Native;
-
     internal class SpikeStripsScript : BaseScript
     {
-        public static bool DoDebugDrawing { get; set; } = true;
-
         public readonly List<SpikeStrip> SpikesStrips = new List<SpikeStrip>();
         public readonly List<Vehicle> PlayersVehicles = new List<Vehicle>();
-
         private DateTime lastPlayersVehiclesUpdateTime = DateTime.UtcNow;
+        PoliceManagement pm = new PoliceManagement();
 
-        /// <summary>
-        /// Checks if the player can deploy spike strips.
-        /// </summary>
-        private bool CanDeploySpikeStrips // TODO: when used in an actual server this should check if the player is a cop or whatever
+        private bool CanDeploySpikeStrips
         {
             get
             {
                 Ped playerPed = LocalPlayer.Character;
+                if (!pm.isCop)
+                {
+                    Screen.ShowNotification("~r~You are not a police officer!", true);
+                    return false;
+                }
+                return !playerPed.IsDead && !playerPed.IsInVehicle() && !playerPed.IsInCover() && !playerPed.IsJumping && !playerPed.IsAiming && !playerPed.IsClimbing;
+            }
+        }
+
+        private bool CanRemoveSpikeStrips
+        {
+            get
+            {
+                Ped playerPed = LocalPlayer.Character;
+                if (!pm.isCop)
+                {
+                    Screen.ShowNotification("~r~You are not a police officer!", true);
+                    return false;
+                }
                 return !playerPed.IsDead && !playerPed.IsInVehicle() && !playerPed.IsInCover() && !playerPed.IsJumping && !playerPed.IsAiming && !playerPed.IsClimbing;
             }
         }
 
         public SpikeStripsScript()
         {
+            while (!NetworkIsPlayerConnected(PlayerId()))
+            {
+                Delay(1);
+            }
             Tick += OnTick;
+            PolM("spike");
         }
+
+        private void PolM(string pluginName)
+        {
+            pm.Checks(pluginName);
+            EventHandlers.Add(pluginName + ":isCop", new Action(pm.IsCop));
+            EventHandlers.Add(pluginName + ":isNotCop", new Action(pm.IsNotCop));
+            EventHandlers.Add(pluginName + ":isAdmin", new Action(pm.IsAdmin));
+            EventHandlers.Add(pluginName + ":isNotAdmin", new Action(pm.IsNotAdmin));
+        }        
 
         private async Task OnTick()
         {
-            if (Game.IsControlPressed(0, Control.Context)) // E + 
+            if (Game.IsControlPressed(0, Control.Context))
             {
-                if (Game.IsControlJustPressed(0, Control.SelectWeaponMelee)) // D2
+                if (Game.IsControlJustPressed(0, Control.SelectWeaponMelee))
                 {
                     if (CanDeploySpikeStrips)
-                    {
                         CreateSpikeStrips();
-                    }
                 }
-                else if (Game.IsControlJustPressed(0, Control.SelectWeaponUnarmed)) // D1
+                else if (Game.IsControlJustPressed(0, Control.SelectWeaponUnarmed))
                 {
-                    DeleteAllSpikeStrips();
+                    if (CanDeploySpikeStrips)
+                        CreateSSpikeStrips();
+                }
+                else if (Game.IsControlJustPressed(0, Control.SelectWeaponShotgun))
+                {
+                    if(CanRemoveSpikeStrips)
+                        DeleteAllSpikeStrips();
                 }
             }
-
-
-            if (SpikesStrips.Count > 0 && (DateTime.UtcNow - lastPlayersVehiclesUpdateTime).TotalSeconds > 1.0) // get players vehicles every second, the native GET_CLOSEST_VEHICLE doesn't return players vehicles
+            if (SpikesStrips.Count > 0 && (DateTime.UtcNow - lastPlayersVehiclesUpdateTime).TotalSeconds > 1.0)
             {
                 PlayersVehicles.Clear();
                 foreach (Player player in Players)
                 {
                     Ped ped = player.Character;
-
                     if (ped.IsInVehicle())
                     {
                         PlayersVehicles.Add(ped.CurrentVehicle);
                     }
                 }
-
                 lastPlayersVehiclesUpdateTime = DateTime.UtcNow;
             }
-
-
-
             for (int i = 0; i < SpikesStrips.Count; i++)
             {
                 SpikesStrips[i].OnTick(PlayersVehicles);
             }
-
-
             await Task.FromResult(0);
         }
 
-        /// <summary>
-        /// Creates two spike strips in-front of the player.
-        /// </summary>
+        private async void CreateSSpikeStrips()
+        {
+            Ped playerPed = LocalPlayer.Character;
+            Vector3 playerPos = playerPed.Position;
+            Vector3 playerForwardVector = playerPed.ForwardVector;
+            float playerHeading = playerPed.Heading;
+            const int SpikeStripsToSpawn = 2;
+            for (int i = 0; i < SpikeStripsToSpawn; i++)
+            {
+                const float SeparationFromPlayer = 2f;
+                const float SeparationBetweenStingers = 3.7f;
+                SpikeStrip s = await SpikeStrip.Create(playerPos + playerForwardVector * (SeparationFromPlayer + (SeparationBetweenStingers * i)), playerHeading);
+                SpikesStrips.Add(s);
+            }
+            Screen.ShowNotification("~g~Deployed 1 spike strip!", true);
+        }
+
         private async void CreateSpikeStrips()
         {
             Ped playerPed = LocalPlayer.Character;
             Vector3 playerPos = playerPed.Position;
             Vector3 playerForwardVector = playerPed.ForwardVector;
             float playerHeading = playerPed.Heading;
-
-            const int SpikeStripsToSpawn = 2;
-
+            const int SpikeStripsToSpawn = 4;
             for (int i = 0; i < SpikeStripsToSpawn; i++)
             {
-                const float SeparationFromPlayer = 3.4f;
-                const float SeparationBetweenStingers = 4.825f;
+                const float SeparationFromPlayer = 2f;
+                const float SeparationBetweenStingers = 3.7f;
 
                 SpikeStrip s = await SpikeStrip.Create(playerPos + playerForwardVector * (SeparationFromPlayer + (SeparationBetweenStingers * i)), playerHeading);
                 SpikesStrips.Add(s);
             }
-
-            Screen.ShowNotification("~o~Deployed spike strips!", true);
+            Screen.ShowNotification("~g~Deployed 2 spike strips!", true);
         }
 
         private void DeleteAllSpikeStrips()
         {
-            for (int i = 0; i < SpikesStrips.Count; i++)
+            if (SpikesStrips.Count != 0)
             {
-                SpikesStrips[i].Prop.Delete();
+                for (int i = 0; i < SpikesStrips.Count; i++)
+                {
+                    SpikesStrips[i].Prop.Delete();
+                }
+                SpikesStrips.Clear();
+                Screen.ShowNotification("~r~Removed spike strips!", true);
             }
-
-            SpikesStrips.Clear();
-
-            Screen.ShowNotification("~r~Removed spike strips!", true);
+            else
+                Screen.ShowNotification("~r~No spike strips deployed!", true);
         }
-
 
         public class SpikeStrip
         {
             private static readonly Model Model = "p_ld_stinger_s";
-
             public Prop Prop { get; private set; }
-
             private SpikeStrip()
             {
             }
-
             [System.Security.SecuritySafeCritical]
             public void OnTick(List<Vehicle> playerVehicles = null)
             {
@@ -133,30 +166,20 @@
                 {
                     Vector3 pointA = Prop.GetOffsetPosition(new Vector3(0.0f, 1.7825f, 0.034225f));
                     Vector3 pointB = Prop.GetOffsetPosition(new Vector3(0.0f, -1.7825f, 0.034225f));
-
                     Vector3 propPos = Prop.Position;
-
                     if (playerVehicles != null)
                     {
                         for (int i = 0; i < playerVehicles.Count; i++)
                         {
                             Vehicle veh = playerVehicles[i];
-
                             if (Vector3.DistanceSquared(veh.Position, propPos) < 7.25f * 7.25f)
                             {
                                 BurstVehicleTyres(veh, pointA, pointB);
                             }
                         }
                     }
-
-                    Vehicle closestVeh = new Vehicle(Function.Call<int>(Hash.GET_CLOSEST_VEHICLE, propPos.X, propPos.Y, propPos.Z, 10.0f, 0, 70));
-
+                    Vehicle closestVeh = new Vehicle(GetClosestVehicle(propPos.X, propPos.Y, propPos.Z, 10.0f, 0, 70));
                     BurstVehicleTyres(closestVeh, pointA, pointB);
-
-                    if (DoDebugDrawing)
-                    {
-                        Function.Call(Hash.DRAW_LINE, pointA.X, pointA.Y, pointA.Z, pointB.X, pointB.Y, pointB.Z, 255, 0, 0, 255);
-                    }
                 }
             }
 
@@ -167,7 +190,6 @@
                     OutputArgument outModelMin = new OutputArgument(), outModelMax = new OutputArgument();
                     Function.Call(Hash.GET_MODEL_DIMENSIONS, v.Model.Hash, outModelMin, outModelMax);
                     Vector3 modelMin = outModelMin.GetResult<Vector3>();
-
                     BurstTyre(v, "wheel_lf", 0, modelMin, pointA, pointB);
                     BurstTyre(v, "wheel_rf", 1, modelMin, pointA, pointB);
                     BurstTyre(v, "wheel_lm1", 2, modelMin, pointA, pointB);
@@ -187,21 +209,13 @@
                 if (CanBurstTyre(veh, wheelBone, wheelIndex))
                 {
                     Vector3 wheelPos = Vector3.Zero;
-
                     wheelPos = veh.Bones[wheelBone].Position;
                     wheelPos.Z += (vehModelMinDim.Z / 2);
-
                     Vector3 wheelClosestPoint = GetClosestPointOnLineSegment(pointA, pointB, wheelPos);
                     float wheelClosestPointSqrDistance = Vector3.DistanceSquared(wheelPos, wheelClosestPoint);
-
                     if (wheelClosestPointSqrDistance < 0.275f * 0.275f)
                     {
-                        Function.Call(Hash.SET_VEHICLE_TYRE_BURST, veh.Handle, wheelIndex, false, 940f);
-                    }
-
-                    if (DoDebugDrawing)
-                    {
-                        Function.Call(Hash.DRAW_LINE, wheelPos.X, wheelPos.Y, wheelPos.Z, wheelClosestPoint.X, wheelClosestPoint.Y, wheelClosestPoint.Z, 0, 255, 0, 255);
+                        SetVehicleTyreBurst(veh.Handle, wheelIndex, false, 940f);
                     }
                 }
             }
@@ -210,31 +224,23 @@
             {
                 Vector3 lineDiffVect = linePointEnd - linePointStart;
                 float lineSegSqrLength = lineDiffVect.LengthSquared();
-
                 Vector3 lineToPointVect = testPoint - linePointStart;
                 float dotProduct = Vector3.Dot(lineDiffVect, lineToPointVect);
-
                 float percAlongLine = dotProduct / lineSegSqrLength;
-
                 if (percAlongLine < 0.0f)
-                {
                     return linePointStart;
-                }
                 else if (percAlongLine > 1.0f)
-                {
                     return linePointEnd;
-                }
-
                 return (linePointStart + (percAlongLine * (linePointEnd - linePointStart)));
             }
 
-
             public static async Task<SpikeStrip> Create(Vector3 position, float heading)
             {
-                SpikeStrip s = new SpikeStrip();
-                s.Prop = await World.CreateProp(Model, position, true, true);
+                SpikeStrip s = new SpikeStrip
+                {
+                    Prop = await World.CreateProp(Model, position, true, true)
+                };
                 s.Prop.Heading = heading;
-
                 return s;
             }
         }
